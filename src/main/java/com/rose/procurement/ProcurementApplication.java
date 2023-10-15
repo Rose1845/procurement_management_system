60efe5794c4c279f6088c73a499bd7865174de42
Rose1845 <odhiamborose466@gmail.com>
2023-10-15 14:27:32 +0300

    initialize the project

---
+++ src/main/java/com/rose/procurement/ProcurementApplication.java
@@ -0,0 +1,13 @@
+package com.rose.procurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rose/procurement/ProcurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rose.procurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
