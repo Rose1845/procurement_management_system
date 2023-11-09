1562e0d2559120e385c590bd7d3870acf06be282
Rose1845 <odhiamborose466@gmail.com>
2023-11-09 22:34:02 +0300

    add get mapping to list all contracts and get single contract. Ability to update and delete(revoke) contract

--- src/main/java/com/rose/procurement/contract/controller/ContractController.java
+++ src/main/java/com/rose/procurement/contract/controller/ContractController.java
@@ -4,10 +4,10 @@ package com.rose.procurement.contract.controller;
 import com.rose.procurement.contract.entities.Contract;
 import com.rose.procurement.contract.request.ContractRequest;
 import com.rose.procurement.contract.service.ContractService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/contract")
@@ -21,4 +21,21 @@ public class ContractController {
     public Contract createContract(@RequestBody ContractRequest contractRequest){
         return contractService.createContract(contractRequest);
     }
+    @GetMapping
+    public List<Contract> getAllContracts(){
+        return contractService.getAllContracts();
+    }
+    @GetMapping("{id}")
+    public Optional<Contract> getContract(@PathVariable("id") String contractId){
+        return contractService.getContract(contractId);
+    }
+    @DeleteMapping("{id}")
+    public String deleteContract(@PathVariable("id") String contractId){
+        return contractService.deleteContract(contractId);
+    }
+    @PutMapping("{id}")
+    public Contract updateContract(@PathVariable("id") String contractId,@RequestBody ContractRequest contractRequest){
+        return contractService.updateContract(contractId,contractRequest);
+    }
+
 }
