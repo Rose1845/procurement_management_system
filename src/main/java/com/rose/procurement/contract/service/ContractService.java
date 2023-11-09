e88bc52ef4a7ed7a31a4562202a550773e709495
Rose1845 <odhiamborose466@gmail.com>
2023-11-09 22:34:41 +0300

    add get mapping to list all contracts and get single contract. Ability to update and delete(revoke) contract service

--- src/main/java/com/rose/procurement/contract/service/ContractService.java
+++ src/main/java/com/rose/procurement/contract/service/ContractService.java
@@ -6,23 +6,18 @@ import com.rose.procurement.contract.request.ContractRequest;
 import com.rose.procurement.supplier.entities.Supplier;
 import com.rose.procurement.supplier.repository.SupplierRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class ContractService {
     private final SupplierRepository supplierRepository;
     private final ContractRepository contractRepository;
-
     public ContractService(SupplierRepository supplierRepository, ContractRepository contractRepository) {
         this.supplierRepository = supplierRepository;
         this.contractRepository = contractRepository;
     }
-
-
     public Contract createContract(ContractRequest contractRequest) {
         Optional<Supplier> supplier = supplierRepository.findByVendorId(contractRequest.getSupplier().getVendorId());
         if(supplier.isEmpty()){
@@ -36,6 +31,34 @@ public class ContractService {
                 .contractType(contractRequest.getContractType())
                 .supplier(supplier.get())
                 .build();
+        return contractRepository.save(contract);
+
+    }
+
+    public List<Contract> getAllContracts(){
+        return new ArrayList<>(contractRepository.findAll()) ;
+    }
+    public String deleteContract(String contractId){
+        Optional<Contract> contract = contractRepository.findById(contractId);
+        if(contract.isPresent()){
+            contractRepository.deleteById(contractId);
+        } else {
+            return "contract with id " + contractId + "do not exist";
+        }
+        return "contract deleted successfully";
+    }
+    public Optional<Contract> getContract(String contractId){
+       return  contractRepository.findById(contractId);
+    }
+
+    public Contract updateContract(String contractId,ContractRequest contractRequest) {
+        Contract contract = contractRepository.findById(contractId).orElseThrow();
+            contract.setContractEndDate(contractRequest.getContractEndDate());
+            contract.setContractTitle(contractRequest.getContractTitle());
+            contract.setContractType(contractRequest.getContractType());
+            contract.setSupplier(contractRequest.getSupplier());
+            contract.setTermsAndConditions(contractRequest.getTermsAndConditions());
+            contract.setContractStartDate(contractRequest.getContractStartDate());
         return contractRepository.save(contract);
 
     }
