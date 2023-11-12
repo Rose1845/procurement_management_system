3cbcffa2fb5b7f9a7a9339feed283e0f3fec86a4
Rose1845 <odhiamborose466@gmail.com>
2023-11-12 16:41:10 +0300

    add dto for INvoice

---
+++ src/main/java/com/rose/procurement/invoice/InvoiceDto.java
@@ -0,0 +1,19 @@
+package com.rose.procurement.invoice;
+
+import com.rose.procurement.purchaseOrder.entities.PurchaseOrder;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * DTO for {@link Invoice}
+ */
+@Data
+@Builder
+public class InvoiceDto{
+    private String invoiceNumber;
+    private LocalDate dueDate;
+    private double totalAmount;
+    private  PurchaseOrder purchaseOrder;
+}
