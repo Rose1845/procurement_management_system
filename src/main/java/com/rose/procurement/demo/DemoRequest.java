b1ce92fd03ae18f9e87062f8470aa3e2d89c3a25
Rose1845 <odhiamborose466@gmail.com>
2023-12-21 17:33:22 +0300

    add api for request demo

---
+++ src/main/java/com/rose/procurement/demo/DemoRequest.java
@@ -0,0 +1,35 @@
+package com.rose.procurement.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DemoRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String phoneNumber;
+    private Date requestDate;
+
+    // Getters and setters
+
+
+}
